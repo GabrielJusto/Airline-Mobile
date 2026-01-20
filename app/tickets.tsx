ad2c02a503b42cfd7985761d27769b966a451767
Gabriel Bonatto Justo <gabriel.justo@acad.pucrs.br>
2026-01-20 10:40:50 -0300

    Add FlightDateCarousel component.

--- app/tickets.tsx
+++ app/tickets.tsx
@@ -2,29 +2,10 @@ import { LeftArrowSvg } from "@/components/svg";
 import { Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
 import { getHeaderTextStyle, globalStyles } from "@/styles/global.styles";
 import { colors } from '../styles/global.styles';
-import { WorkSans_400Regular } from "@expo-google-fonts/work-sans";
+import { FlightDateCarousel } from "@/components/FlightDateCarousel";
 
 
 export default function Tickets() {
-
-    interface FlightDate {
-        id: number,
-        date: Date,
-        selected: boolean
-    }
-    const dates: FlightDate[] = [];
-    for (let i = -5; i < 10; i++) 
-    {
-        const date = new Date();
-        date.setDate(date.getDate() + i);
-        dates.push({
-            id: i+5,
-            date: date,
-            selected: i == 0 
-        });
-    }
-
-
     return (
         <View style={style.container}>
             <View style={globalStyles.headerContainer}>
@@ -35,20 +16,7 @@ export default function Tickets() {
                     Flights
                 </Text>
             </View>
-            <ScrollView horizontal={true} contentContainerStyle={style.calendarContainer}>
-                {dates.map(function(flightDate: FlightDate){
-                    return (
-                        <View style={[style.dateContainer, flightDate.selected ? style.selectedDate : null]}>
-                            <Text style={[style.weekDayText, flightDate.selected ? style.selectedWeekDayText : null]}>
-                                {flightDate.date.toLocaleDateString('en-US', { weekday: 'short' }).toUpperCase()}
-                            </Text>
-                            <Text style={[style.monthDayText, flightDate.selected ? style.selectedMonthDayText : null]}>
-                                {flightDate.date.getDate()}
-                            </Text>
-                        </View>
-                    )
-                })}
-            </ScrollView>
+            <FlightDateCarousel/>
             <View style={style.ticketsContainer}>
 
             </View>
@@ -62,42 +30,7 @@ const style = StyleSheet.create({
     container: {
 
     },
-    calendarContainer: {
-        gap: 12
-    },
     ticketsContainer: {
 
-    },
-    selectedDate:{
-        borderColor: colors.primary,
-        borderWidth: 2
-    },
-    dateContainer:{
-        borderWidth: 1,
-        borderColor: colors.gray,
-        borderRadius: 14,
-        paddingHorizontal: 15,
-        paddingVertical: 10,
-        alignItems: "center",
-        justifyContent: "center",
-        width: 55
-    },
-    weekDayText:{
-        fontFamily: WorkSans_400Regular.toString(),
-        fontSize: 12,
-        color: colors.gray,
-        textAlign: "center"
-    },
-    selectedWeekDayText:{
-        color: colors.primary,
-    },
-    monthDayText:{
-        fontFamily: WorkSans_400Regular.toString(),
-        fontSize: 24,
-        color: colors.gray,
-        textAlign: "center"
-    },
-    selectedMonthDayText:{
-        color: colors.primary,
     }
 });
---
+++ components/FlightDateCarousel/index.tsx
@@ -0,0 +1,93 @@
+import { colors } from "@/styles/global.styles";
+import { WorkSans_400Regular } from "@expo-google-fonts/work-sans";
+import { useState } from "react";
+import { Pressable, ScrollView, StyleSheet, Text } from "react-native"
+
+interface FlightDate {
+    id: number,
+    date: Date,
+    selected: boolean
+}
+
+function initFlightDates(): FlightDate[] {
+    const dates: FlightDate[] = [];
+    for (let i = -5; i < 10; i++) {
+        const date = new Date();
+        date.setDate(date.getDate() + i);
+        dates.push({
+            id: i + 5,
+            date: date,
+            selected: i === 0
+        });
+    }
+    return dates;
+}
+
+export const FlightDateCarousel = () => {
+
+
+    const [datesState, setDatesState] = useState(initFlightDates());
+
+    function setDate(dateId: number): void {
+        setDatesState(prevDates => prevDates.map(date => (
+            {
+                ...date,
+                selected: date.id === dateId
+            }
+        )));
+    }
+    return (
+        <ScrollView horizontal={true} contentContainerStyle={style.calendarContainer}>
+            {datesState.map(function (flightDate: FlightDate) {
+                return (
+                    <Pressable key={flightDate.id} onPress={() => setDate(flightDate.id)} style={[style.dateContainer, flightDate.selected ? style.selectedDate : null]}>
+                        <Text style={[style.weekDayText, flightDate.selected ? style.selectedWeekDayText : null]}>
+                            {flightDate.date.toLocaleDateString('en-US', { weekday: 'short' }).toUpperCase()}
+                        </Text>
+                        <Text style={[style.monthDayText, flightDate.selected ? style.selectedMonthDayText : null]}>
+                            {flightDate.date.getDate()}
+                        </Text>
+                    </Pressable>
+                )
+            })}
+        </ScrollView>
+    )
+}
+
+const style = StyleSheet.create({
+    calendarContainer: {
+        gap: 12
+    },
+    selectedDate: {
+        borderColor: colors.primary,
+        borderWidth: 2
+    },
+    dateContainer: {
+        borderWidth: 1,
+        borderColor: colors.gray,
+        borderRadius: 14,
+        paddingHorizontal: 15,
+        paddingVertical: 10,
+        alignItems: "center",
+        justifyContent: "center",
+        width: 55
+    },
+    weekDayText: {
+        fontFamily: WorkSans_400Regular.toString(),
+        fontSize: 12,
+        color: colors.gray,
+        textAlign: "center"
+    },
+    selectedWeekDayText: {
+        color: colors.primary,
+    },
+    monthDayText: {
+        fontFamily: WorkSans_400Regular.toString(),
+        fontSize: 24,
+        color: colors.gray,
+        textAlign: "center"
+    },
+    selectedMonthDayText: {
+        color: colors.primary,
+    }
+});
