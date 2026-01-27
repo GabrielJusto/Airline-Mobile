0d9ffbb794ba851bfddd8871c48854a172bd8d6a
Gabriel Bonatto Justo <gabriel.justo@acad.pucrs.br>
2026-01-27 15:04:28 -0300

    Finish ticket sale screen.

--- components/StoreFlight/index.tsx
+++ components/StoreFlight/index.tsx
@@ -1,12 +1,12 @@
 import { Flight } from "@/interfaces/Flight";
-import { StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 import { AirplaneSvg, FlightDurationSvg } from "../svg";
 import { WorkSans_400Regular } from "@expo-google-fonts/work-sans";
 import { colors } from "@/styles/global.styles";
 
 
 
-export default function StoreFlight ({ flight }: { flight: Flight }) {
+export default function StoreFlight({ flight }: { flight: Flight }) {
     return (
         <View style={style.container}>
             <View style={style.row}>
@@ -18,9 +18,11 @@ export default function StoreFlight ({ flight }: { flight: Flight }) {
                         {flight.originCity}
                     </Text>
                 </View>
-                <AirplaneSvg style={style.airplaneSvg} fill="#000"/>
                 <View style={style.airport}>
-                    <Text  style={style.airportCodeText}>
+                    <AirplaneSvg style={style.airplaneSvg} fill="#000" />
+                </View>
+                <View style={style.airport}>
+                    <Text style={style.airportCodeText}>
                         {flight.detinationAirportCode}
                     </Text>
                     <Text style={style.infoText}>
@@ -28,7 +30,7 @@ export default function StoreFlight ({ flight }: { flight: Flight }) {
                     </Text>
                 </View>
             </View>
-            <View style={style.row}>
+            <View style={[style.row, style.timeRow]}>
                 <View style={style.airport}>
                     <Text style={style.infoTitleText}>
                         Date
@@ -38,7 +40,7 @@ export default function StoreFlight ({ flight }: { flight: Flight }) {
                     </Text>
                 </View>
                 <View style={style.airport}>
-                    <FlightDurationSvg/>
+                    <FlightDurationSvg style={style.airplaneSvg} />
                     <Text style={style.flightDurationText}>
                         {flight.flightDuration}
                     </Text>
@@ -52,42 +54,89 @@ export default function StoreFlight ({ flight }: { flight: Flight }) {
                     </Text>
                 </View>
             </View>
+            <View style={style.row}>
+                <View style={style.airport}>
+                    <Text style={style.priceDescription}>
+                        Ticket Price:
+                    </Text>
+                    <Text style={style.priceText}>
+                        R$ {flight.price.toFixed(2).replace('.', ',')}
+                    </Text>
+                </View>
+                <View style={style.airport}>
+
+                </View>
+                <View style={style.airport}>
+                    <Pressable style={style.buyButton}>
+                        <Text style={style.priceText}>
+                            Buy
+                        </Text>
+                    </Pressable>
+                </View>
+            </View>
         </View>
     );
 }
 
 const style = StyleSheet.create({
-    container:{
+    buyButton: {
+        backgroundColor: colors.orange,
+        paddingHorizontal: 24,
+        paddingVertical: 8,
+        borderRadius: 8,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    priceDescription: {
+        fontFamily: WorkSans_400Regular.toString(),
+        fontSize: 12,
+        color: colors.gray
+    },
+    priceText: {
+        fontFamily: WorkSans_400Regular.toString(),
+        fontSize: 14,
+        color: colors.darkText
+    },
+    timeRow: {
+        borderBottomWidth: 1,
+        borderBottomColor: "#000"
+    },
+    priceContainer: {
+        gap: 10,
+        flexDirection: "row",
+        maxWidth: "50%"
+    },
+    container: {
         backgroundColor: "#FFF",
         borderRadius: 28,
-        height: 205,
+        // height: 240,
         width: "100%",
-        paddingHorizontal:25,
-        paddingVertical:17,
-        gap:35
+        paddingHorizontal: 25,
+        paddingVertical: 17,
+        gap: 12
     },
-    row:{
+    row: {
         flexDirection: "row",
         gap: 55,
         alignItems: "center",
         justifyContent: "center"
     },
-    airport:{
-        flex:1,
-        alignItems:"center",
+    airport: {
+        flex: 1,
+        alignItems: "center",
         justifyContent: "center",
         textAlign: "center"
     },
-    airplaneSvg:{
+    airplaneSvg: {
         height: 48,
         width: 48
     },
-    airportCodeText:{
+    airportCodeText: {
         fontFamily: WorkSans_400Regular.toString(),
         fontSize: 28,
         color: colors.primary
     },
-    cityText:{
+    cityText: {
         fontFamily: WorkSans_400Regular.toString(),
         fontSize: 12,
     },
@@ -96,13 +145,14 @@ const style = StyleSheet.create({
         fontFamily: WorkSans_400Regular.toString(),
         color: colors.gray
     },
-    infoTitleText:{
+    infoTitleText: {
         fontFamily: WorkSans_400Regular.toString(),
         color: colors.darkText
     },
-    flightDurationText:{
+    flightDurationText: {
         fontFamily: WorkSans_400Regular.toString(),
-        color: colors.primary
+        color: colors.primary,
+        marginBottom: 5
     }
 
 })
